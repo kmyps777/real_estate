1cdac037bdfcbcfe4aa329e8920a9da595d2547d
kmyps777 <47943705+kmyps777@users.noreply.github.com>
2026-03-12 22:11:16 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "realty-v1";
+const FILES = [
+  "./index.html",
+  "./manifest.json",
+  "https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/7.23.2/babel.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/recharts/2.8.0/Recharts.min.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
